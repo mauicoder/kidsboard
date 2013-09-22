ff6a6d67a286a9e661a8df65ac2c74754231d1a2
Maurizio Lo Bosco <maurizio.lobosco@gmail.com>
2013-09-22 18:47:12 +0200

    added stroke path examples in comments

--- src/net/maui/kidsboard/model/PaintModel.java
+++ src/net/maui/kidsboard/model/PaintModel.java
@@ -2,12 +2,15 @@ package net.maui.kidsboard.model;
 
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 
 public class PaintModel {
 
 	private static PaintModel instance = new PaintModel();
 	
 	private volatile Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	
+	private float mPhase = 3;
 	
 	public static PaintModel getInstance(){
 		return instance;
@@ -16,10 +19,27 @@ public class PaintModel {
 	private Paint getDefaultPaint(){
 		Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
 		p.setStyle(Paint.Style.STROKE);
+		//p.setPathEffect(new PathDashPathEffect(makePathDash(), 12, mPhase, PathDashPathEffect.Style.MORPH));
+		//p.setPathEffect(new CornerPathEffect(10));
+		//p.setPathEffect(new DashPathEffect(new float[] {10,10}, 0)); 
 		p.setStrokeWidth(5);
 		p.setColor(Color.BLACK);
 		return p;
 	}
+	
+	private static Path makePathDash() {
+        Path p = new Path();
+        p.moveTo(-6, 4);
+        p.lineTo(6,4);
+        p.lineTo(6,3);
+        p.lineTo(-6, 3);
+        p.close();
+        p.moveTo(-6, -4);
+        p.lineTo(6,-4);
+        p.lineTo(6,-3);
+        p.lineTo(-6, -3);
+        return p;
+    }
 	
 	public Paint setDefaultPaint(){
 		setPaint(getDefaultPaint());
@@ -40,5 +60,12 @@ public class PaintModel {
 	
 	public void setColor(int color){
 		paint.setColor(color);
+		setStrokeWidth(color);
+	}
+	private void setStrokeWidth(int color){
+		if (color == Color.WHITE) {
+			paint.setStrokeWidth(15);
+		} else
+			paint.setStrokeWidth(5);
 	}
 }
